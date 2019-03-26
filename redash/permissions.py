cd95cc6dab61ce8b45a6f8b98a71c5d2886d7139
Omer Lachish <omer@rauchy.net>
2019-03-26 15:09:00 +0200

    allow access to a query by the api_key of the dashboard which includes it

--- redash/permissions.py
+++ redash/permissions.py
@@ -16,12 +16,16 @@ ACCESS_TYPES = (ACCESS_TYPE_VIEW, ACCESS_TYPE_MODIFY, ACCESS_TYPE_DELETE)
 
 def has_access(object, user, need_view_only):
     if hasattr(object, 'api_key') and user.is_api_user():
-        return has_access_to_object(object, user, need_view_only)
+        return has_access_to_object(object, user.id, need_view_only)
     else:
         return has_access_to_groups(object.groups, user, need_view_only)
 
-def has_access_to_object(object, user, need_view_only):
-    return (object.api_key == user.id) and need_view_only
+def has_access_to_object(object, api_key, need_view_only):
+    if object.api_key == api_key:
+        return need_view_only
+    else:
+        # check if api_key belongs to a dashboard containing this query
+        return api_key in object.dashboard_api_keys and need_view_only
 
 def has_access_to_groups(groups, user, need_view_only):
     if 'admin' in user.permissions:
